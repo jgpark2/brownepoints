7e063c3ddeb0881720650e418df19ebc85b3bfb5
Grindlemire <grindlemire@yahoo.com>
2014-03-20 03:43:52 -0500

    We have all the basic functionality done for the first demo I believe

--- src/com/example/BrownEPoints/user_homepage_activity.java
+++ src/com/example/BrownEPoints/user_homepage_activity.java
@@ -1,13 +1,20 @@
 package com.example.BrownEPoints;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class user_homepage_activity extends Activity implements View.OnClickListener{
+
+
+    private static String search_word = "";
 
     /**
      * Called when the activity is first created.
@@ -16,6 +23,12 @@ public class user_homepage_activity extends Activity implements View.OnClickList
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_homepage);
+
+        SearchView searchView= (SearchView)findViewById(R.id.user_search);
+        int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        TextView textView = (TextView) searchView.findViewById(id);
+        textView.setTextColor(Color.BLACK);
+
     }
 
     public void onClick(View v){}
@@ -36,6 +49,55 @@ public class user_homepage_activity extends Activity implements View.OnClickList
         login_activity.logout_user();
         startActivity(new Intent(user_homepage_activity.this, login_activity.class));
         Toast.makeText(this, "You have been logged out.", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public static String get_search_result(){
+        return search_word;
+    }
+
+
+
+
+    public void search_user_database_button(View v){
+        SearchView search_result = (SearchView) findViewById(R.id.user_search);
+        final Context theContext = this;
+
+        search_result.setSubmitButtonEnabled(true);
+
+        search_result.setOnQueryTextListener( new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                search_word = query;
+
+                if(search_word.equals("")){
+                    //Toast.makeText(this, "Please input something to search", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+
+                String sRequest = "http://web.engr.illinois.edu/~null_ptrs/bpoints/user_table/select_user.php?username="+search_word;
+                String output = Process_request.runProcess(sRequest);
+
+                if (output.equals(""))
+                {
+                    Toast.makeText(theContext, "No user with that username", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+
+                else
+                {
+                    startActivity(new Intent(user_homepage_activity.this, user_search_results_activity.class));
+                    return true;
+                }
+            }
+        }
+        );
 
     }
 
---
+++ src/com/example/BrownEPoints/user_search_results_activity.java
@@ -0,0 +1,80 @@
+package com.example.BrownEPoints;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class user_search_results_activity extends Activity implements View.OnClickListener{
+
+    private Map<String,String> search_user_info = new HashMap<String, String>();
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_search_results);
+
+        String search_word  = user_homepage_activity.get_search_result();
+
+        TextView user_name = (TextView)findViewById(R.id.search_user_info_username);
+        TextView user_age = (TextView)findViewById(R.id.search_user_info_age);
+        TextView user_country = (TextView)findViewById(R.id.search_user_info_country);
+        TextView user_state = (TextView)findViewById(R.id.search_user_info_state);
+        TextView user_ethnicity = (TextView)findViewById(R.id.search_user_info_ethnicity);
+        TextView user_gender = (TextView)findViewById(R.id.search_user_info_gender);
+
+        String sRequest = "http://web.engr.illinois.edu/~null_ptrs/bpoints/user_table/select_user.php?username="+search_word;
+        Log.w("sRequest", sRequest);
+        String output = Process_request.runProcess(sRequest);
+        System.out.println(output);
+        int prev = 0;
+        try{
+
+
+            while(prev<output.length())
+            {
+                int start_type = output.indexOf("|", prev);
+                int end_type = output.indexOf("=", prev);
+
+                if(start_type<0 || end_type <0)
+                    break;
+
+                String input_key = output.substring(start_type+1, end_type);
+                prev = end_type;
+
+
+                int start_info = output.indexOf("=", prev);
+                int end_info = output.indexOf("|", prev);
+                String input_value = output.substring(start_info+1, end_info);
+                prev++;
+
+                search_user_info.put(input_key, input_value);
+
+            }
+        }
+        catch (NullPointerException n){
+            n.printStackTrace();
+        }
+
+        user_name.setText(search_user_info.get("Username"));
+        user_age.setText(search_user_info.get("Age"));
+        user_country.setText(search_user_info.get("Country"));
+        user_state.setText(search_user_info.get("State"));
+        user_ethnicity.setText(search_user_info.get("Ethnicity"));
+        user_gender.setText(search_user_info.get("Gender"));
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
