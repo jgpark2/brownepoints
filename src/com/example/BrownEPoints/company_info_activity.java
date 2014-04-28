268dcf6c4bcc01e6210322cb928e85abbf30e1d8
holsten2 <vizzlestein@gmail.com>
2014-04-28 00:55:53 +0000

    added information to get demographic

--- src/com/example/BrownEPoints/company_info_activity.java
+++ src/com/example/BrownEPoints/company_info_activity.java
@@ -35,15 +35,20 @@ public class company_info_activity extends Activity {
         String company_rating = Process_request.runProcess(average_company_rating);
 
 
+        String targeted_ethnicity = "https://web.engr.illinois.edu/~null_ptrs/bpoints/company_table/get_demographic_ethnicity.php?CompanyID="+companyId;
+        String max_ethnicity = Process_request.runProcess(targeted_ethnicity);
+
         TextView AverageAgeTV = (TextView)findViewById(R.id.average_age_input);
         TextView AverageCompanyRatingTV = (TextView)findViewById(R.id.average_company_rating_input);
         TextView GenderRatioTV = (TextView)findViewById(R.id.gender_ratio_input);
         TextView AverageAdRatingTV = (TextView)findViewById(R.id.average_ad_rating_input);
+        TextView MaxEthnicity = (TextView)findViewById(R.id.targeted_ethnicity);
 
         AverageAgeTV.setText(age_request_output);//company_table/get_average_user_age_of_company.php
         AverageCompanyRatingTV.setText(company_rating);//company_table/get_company_average.php
         AverageAdRatingTV.setText(ad_rating);//company_table/get_average_ad_rating.php
         GenderRatioTV.setText(genderRatio);//company_table/get_gender_ratio
+        MaxEthnicity.setText(max_ethnicity);//company_table/get_demographic_ethnicity.php
 
         //does this work
         //branch working?
